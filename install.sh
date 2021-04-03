e83a7e898bd3cb2b07e0a2f8e5218efadcafe1f8
Judzhin Miles <info@msbios.com>
2021-04-03 03:12:40 +0300

    Add Install Script

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+echo "[01] Clone Submodule ZStore\n";
+git submodule update --init --recursive
+
+echo "[02] Builde Docker Containers\n";
 docker-compose up -d --build
+
+echo "[03] Install Project Dependencies\n";
+docker-compose run --rm php-apache composer install
+
+echo "[04] Import Database Structure\n";
+cat ./zstore/db/db.sql | docker-compose exec -T mariadb mysql -uroot -proot zstore.dev
+
+echo "[05] Import Project Data\n";
+cat ./zstore/db/initdata.sql | docker-compose exec -T mariadb mysql -uroot -proot zstore.dev
